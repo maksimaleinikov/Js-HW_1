dac38615af2d3c95e55a6c0385d776293a8028da
unknown <april2108@rambler.ru>
2020-02-05 17:02:36 +0300

    release v1

---
+++ main.js
@@ -0,0 +1,30 @@
+alert('qwer');
+let Dad = {
+    name: 'Jackie',
+    age: 32,
+    isInfected: false,
+    willBeSaved: null
+}
+
+let Mom = {
+    name: 'Lussy Li',
+    age: 29,
+    isInfected: false,
+    willBeSaved: null
+}
+
+let Son = {
+    name: 'Mao',
+    age: 5,
+    isInfected: true,
+    willBeSaved: null
+}
+
+let Dog = {
+    name: 'Chappie',
+    age: 3,
+    isInfected: true,
+    willBeSaved: false
+}
+let happyChineeseFamily = [Dad, Mom, Son, Dog];
+console.log(happyChineeseFamily);
